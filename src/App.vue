346169d0e9fbc9dffb3a99ba45132458988aa6c3
Shaoqing Tan <tim.tan@hydiant.com>
2019-01-10 00:36:42 -0500

    Make dumb sound

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DumbSound />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DumbSound from "./components/DumbSound.vue";
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    DumbSound
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/DumbSound.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p>Dumb Sound</p>
+    <button @click="play">
+      Doo
+    </button>
+  </div>
+</template>
+
+<script>
+import Tone from "tone";
+const synth = new Tone.Synth({
+  oscillator: {
+    type: "sine4",
+    modulationFrequency: 0.2
+  },
+  envelope: {
+    attack: 0.05,
+    decay: 0.1,
+    sustain: 0.2,
+    release: 0.9
+  }
+}).toMaster();
+
+export default {
+  name: "DumbSound",
+  props: {},
+  data: () => {
+    return {};
+  },
+  methods: {
+    play: () => {
+      synth.triggerAttackRelease("C4", "2n");
+      synth.triggerAttackRelease("E4", "2n");
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
